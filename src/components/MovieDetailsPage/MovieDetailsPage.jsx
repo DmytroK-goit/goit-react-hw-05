f33b2143154c88baec5557d4dde338aca8d12dfd
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2024-09-27 14:09:27 +0300

    1

--- src/components/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/components/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,19 +1,26 @@
-// MovieDetailsPage.js
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
 import { getMovieDetails } from "/Users/dmytro.kovbasiuk/Desktop/HTML/EDU JS/goit-react-hw-05/src/components/api/api"; // Запит для отримання деталей фільму
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 const MovieDetailsPage = () => {
+  const [loading, setLoading] = useState(false);
   const { movieId } = useParams();
   const [movieDetails, setMovieDetails] = useState(null);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from || "/";
 
   useEffect(() => {
     const fetchMovieDetails = async () => {
       try {
         const dataDetails = await getMovieDetails(movieId);
         setMovieDetails(dataDetails);
+        // toast.success(`Успішно виконано!`);
       } catch (error) {
-        console.error("Error fetching movie details:", error);
+        toast.error(`Сталася помилка: ${error.message}`);
       }
     };
 
@@ -26,9 +33,19 @@ const MovieDetailsPage = () => {
 
   return (
     <div>
+      <button onClick={() => navigate(from)}>Назад</button>
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${movieDetails.backdrop_path}`}
+        alt=""
+      />
       <h2>{movieDetails.title}</h2>
       <p>{movieDetails.overview}</p>
       <p>Release date: {movieDetails.release_date}</p>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+      />
     </div>
   );
 };
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,29 +1,48 @@
 import React, { useEffect, useState } from "react";
 import { surchMovie } from "/Users/dmytro.kovbasiuk/Desktop/HTML/EDU JS/goit-react-hw-05/src/components/api/api";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate, Link, useLocation } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const MoviesPage = () => {
   const [searchMovies, setSearchMovies] = useState([]);
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const queryParam = searchParams.get("query");
+
+    if (queryParam) {
+      setQuery(queryParam);
+      fetchMovies(queryParam);
+    }
+  }, [location.search]);
 
   const handleInputChange = (evt) => {
     setQuery(evt.target.value);
   };
 
-  const handleSubmit = async (evt) => {
+  const handleSubmit = (evt) => {
     evt.preventDefault();
 
     if (query.trim() === "") {
-      alert("Please enter a search query");
+      toast.info("Пошуковий запит не введений");
       return;
     }
 
+    navigate(`?query=${query}`);
+    fetchMovies(query);
+  };
+
+  const fetchMovies = async (searchQuery) => {
     try {
-      const data = await surchMovie(query);
+      const data = await surchMovie(searchQuery);
       setSearchMovies(data.results);
+      // toast.success(`Успішно виконано!`);
     } catch (error) {
-      console.error("Error fetching movies:", error);
+      toast.error(`Сталася помилка: ${error.message}`);
     }
   };
 
@@ -43,13 +62,18 @@ const MoviesPage = () => {
         <ul>
           {searchMovies.map((movie) => (
             <li key={movie.id}>
-              <Link to={`/movies/${movie.id}`}>
+              <Link to={`/movies/${movie.id}`} state={{ from: location }}>
                 <p>{movie.original_title}</p>
               </Link>
             </li>
           ))}
         </ul>
       )}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+      />
     </div>
   );
 };
